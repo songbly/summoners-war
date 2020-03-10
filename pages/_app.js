496a2afbd3b889beec4ed305b2f583216ce92782
Hyeseong <ovo1381@gmail.com>
2020-03-11 06:02:24 +0900

    :sparkles: add redux and redux-logger

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,9 @@ import React from "react";
 import withRedux from "next-redux-wrapper";
 import { Provider } from "react-redux";
 import { createStore, compose, applyMiddleware } from "redux";
-import reducer from "../reducers";
+import rootReducer from "../lib/ducks";
 import { composeWithDevTools } from "redux-devtools-extension";
+import logger from "../lib/middlewares/logger";
 
 const Test = ({ Component, store }) => {
   return (
@@ -14,12 +15,12 @@ const Test = ({ Component, store }) => {
 };
 
 const configureStore = (initialState, options) => {
-  const middlewares = []; // 미들웨어들을 넣으면 된다.
+  const middlewares = [logger];
   const enhancer =
     process.env.NODE_ENV === "production"
       ? compose(applyMiddleware(...middlewares))
       : composeWithDevTools(applyMiddleware(...middlewares));
-  const store = createStore(reducer, initialState, enhancer);
+  const store = createStore(rootReducer, initialState, enhancer);
   return store;
 };
 
